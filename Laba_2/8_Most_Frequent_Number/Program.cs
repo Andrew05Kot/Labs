a2a4c4dbfd75173676d18864d6f1a0fea185e094
Andrey <55507306+Andrey5kot@users.noreply.github.com>
2019-09-22 20:19:13 +0300

    Add files via upload

---
+++ Laba_2/8_Most_Frequent_Number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _8_Most_Frequent_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Array: ");
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int mfn=0, n =0, maxN=0;
+
+            for(int i =0; i < arr.Length; i++)
+            {
+                for(int j = i; j < arr.Length;  j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        n++;
+                        if (maxN < n)
+                        {
+                            mfn = arr[i];
+                            maxN = n;
+                        }
+                    }
+                }
+                n = 0;
+            }
+            Console.WriteLine("Most Frequent Number: " + mfn);
+
+        }
+    }
+}
