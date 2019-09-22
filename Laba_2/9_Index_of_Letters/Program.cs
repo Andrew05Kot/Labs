676644ac96041680cdfd57ef8cc3cbdca827ba53
Andrey <55507306+Andrey5kot@users.noreply.github.com>
2019-09-22 20:20:08 +0300

    Add files via upload

---
+++ Laba_2/9_Index_of_Letters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _9_Index_of_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] str = Console.ReadLine().Split().Select(char.Parse).ToArray();
+            int[] num = Enumerable.Range(0, 28).ToArray();
+            char[] letter = new char[] {'a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+
+            for (int i= 0; i < str.Length; i++)
+            {
+                for(int j=0;j<letter.Length; j++)
+                {
+                    if (str[i] == letter[j])
+                    {
+                        Console.WriteLine(str[i] + " -> " + num[j]);
+                    }
+                }
+            }
+        }
+    }
+}
