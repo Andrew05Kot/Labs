a46afbfa3fc54af9aee014f7a4bd4d99316c90de
Andrey <55507306+Andrey5kot@users.noreply.github.com>
2019-09-22 20:17:19 +0300

    Add files via upload

---
+++ Laba_2/6_Max_Sequence_of_Equal_Elements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _6_Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Array: ");
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int msee = 0, n = 0, maxN = 0, q=0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        n++;
+                        if (maxN < n)
+                        {
+                            msee = arr[i];
+                            maxN = n;
+                        }
+                    }
+                    else
+                    {
+                        j = arr.Length;
+                    }
+                }
+                n = 0;
+            }
+            for(int i = 0; i < maxN+1; i++)
+            {
+                Console.Write(msee + " ");
+            }
+
+        }
+    }
+}
