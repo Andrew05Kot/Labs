050024530e10833332bcd7861c9c05f4786e7195
Andrey <55507306+Andrey5kot@users.noreply.github.com>
2019-10-08 15:15:59 +0300

    Add files via upload

---
+++ Laba_3/9_Rectangle_Intersection/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _9_Rectangle_Intersection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] parameters = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int numbeOfRectangles = Convert.ToInt32(parameters[0]);
+            int intersectionChecks = Convert.ToInt32(parameters[0]);
+
+            Rectangle[] rectangles = new Rectangle[numbeOfRectangles];
+
+
+            for (int i = 0; i < numbeOfRectangles; i++)
+            {
+                string[] parameters1 = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string id = parameters1[0];
+                int width = Convert.ToInt32(parameters1[1]);
+                int height = Convert.ToInt32(parameters1[2]);
+                int x = Convert.ToInt32(parameters1[3]);
+                int y = Convert.ToInt32(parameters1[4]);
+                rectangles[i] = new Rectangle(id, width, height, x, y);
+            }
+
+            int first = 0;
+            int second = 0;
+            for (int i = 0; i < intersectionChecks; i++)
+            {
+                string[] parameters2 = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string Id1 = parameters2[0];
+                string Id2 = parameters2[1];
+
+                for (int j = 0; j < numbeOfRectangles; j++)
+                {
+                    if (rectangles[j].id == Id1)
+                    {
+                        first = j;
+                    }
+                    if (rectangles[j].id == Id2)
+                    {
+                        second = j;
+                    }
+                }
+                Console.WriteLine(rectangles[first].Check(rectangles[first], rectangles[second]));
+            }
+        }
+    }
+}
---
+++ Laba_3/9_Rectangle_Intersection/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _9_Rectangle_Intersection
+{
+    class Rectangle
+    {
+        public string id { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Rectangle(string id, int width, int heigth, int x, int y)
+        {
+            this.id = id;
+            this.width = width;
+            this.height = heigth;
+            this.x = x;
+            this.y = y;
+        }
+        public bool Check(Rectangle ractangle1, Rectangle ractangle2)
+        {
+
+            if((ractangle1.x >= ractangle2.x && ractangle1.x <= ractangle2.x + ractangle2.width)
+                || (ractangle1.x + ractangle1.width >= ractangle2.x && ractangle1.x + ractangle1.width <= ractangle2.x + ractangle2.width)
+                || (ractangle1.y + ractangle1.height >= ractangle2.y && ractangle1.y + ractangle1.height <= ractangle2.y + ractangle2.height))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
