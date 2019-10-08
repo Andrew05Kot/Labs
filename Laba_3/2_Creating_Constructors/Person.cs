3a379072186b2b332cce3f0db5837b7f54a3a4ed
Andrey <55507306+Andrey5kot@users.noreply.github.com>
2019-10-08 15:09:17 +0300

    Add files via upload

---
+++ Laba_3/2_Creating_Constructors/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2_Creating_Constructors
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+        }
+        public Person()
+        {
+        }
+        public Person(string n)
+        {
+            name = n;
+        }
+        public Person(string n, int a)
+        {
+            name = n;
+            age = a;
+        }
+    }
+}
---
+++ Laba_3/2_Creating_Constructors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2_Creating_Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person first = new Person();
+            Person second = new Person("No name");
+            Person Peson3 = new Person("Peson3", 100);
+
+            first.Name = "No name";
+            first.Age = 1;
+
+            second.Age = 18;
+
+
+            Console.WriteLine(first.Name + " " + first.Age);
+            Console.WriteLine(second.Name + " " + second.Age);
+            Console.WriteLine(Peson3.Name + " " + Peson3.Age);
+        }
+    }
+}
