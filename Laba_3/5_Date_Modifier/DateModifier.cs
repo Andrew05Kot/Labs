4cdbd580956c2b253ce51ee8dbc393c923fae2d8
Andrey <55507306+Andrey5kot@users.noreply.github.com>
2019-10-08 15:15:12 +0300

    Add files via upload

---
+++ Laba_3/5_Date_Modifier/DateModifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace _5_Date_Modifier
+{
+    class DateModifier
+    {
+        public static double GetDaysBetweenDates(string dateFirst, string dateSecond)
+        {
+            
+            var firstDate = DateTime.ParseExact(dateFirst, "yyyy MM dd", CultureInfo.InvariantCulture);
+            var secondDate = DateTime.ParseExact(dateSecond, "yyyy MM dd", CultureInfo.InvariantCulture);
+
+            if (firstDate > secondDate)
+            {
+                return GetDaysBetweenDates(dateSecond, dateFirst);
+            }
+
+            return (secondDate - firstDate).Days;
+        }
+    }
+}
---
+++ Laba_3/5_Date_Modifier/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace _5_Date_Modifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Date: ");
+            string firstDate = Console.ReadLine();
+            Console.WriteLine("Second Date: ");
+            string secondDate = Console.ReadLine();
+            Console.WriteLine("Number of days: ");
+            Console.WriteLine(DateModifier.GetDaysBetweenDates(firstDate, secondDate));
+        }
+    }
+}
