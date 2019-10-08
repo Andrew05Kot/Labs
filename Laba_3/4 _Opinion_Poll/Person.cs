c88fd8828d39ff58d1ae8a4ef9322ffa4f0b73d4
Andrey <55507306+Andrey5kot@users.noreply.github.com>
2019-10-08 15:10:53 +0300

    Add files via upload

---
+++ Laba_3/4 _Opinion_Poll/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4__Opinion_Poll
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+        }
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            return this.Name + " - " + this.Age;
+        }
+    }
+}
---
+++ Laba_3/4 _Opinion_Poll/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4__Opinion_Poll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the number of people: ");
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
+            var persons = new List<Person>();
+
+            Console.WriteLine("Enter the name and age of people: ");
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                string[] parameters = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                persons.Add(new Person(parameters[0], Convert.ToInt32(parameters[1])));
+            }
+
+            persons
+                .Where(x => x.Age > 30)
+                .OrderBy(x => x.Name)
+                .ToList()
+                .ForEach(x => Console.WriteLine(x));
+        }
+        
+
+
+    }
+}
