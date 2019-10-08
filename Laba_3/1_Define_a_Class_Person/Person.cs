5e913ddda7e3d370a7a6fb4d7aea86904019b87c
Andrey <55507306+Andrey5kot@users.noreply.github.com>
2019-10-08 15:08:32 +0300

    Add files via upload

---
+++ Laba_3/1_Define_a_Class_Person/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_Define_a_Class_Person
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+        }
+
+    }
+}
---
+++ Laba_3/1_Define_a_Class_Person/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1_Define_a_Class_Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Pesho = new Person();
+            Person Gosho = new Person();
+            Person Stamat = new Person();
+
+            Pesho.Name = "Pesho";
+            Pesho.Age = 20;
+            Gosho.Name = "Gosho";
+            Gosho.Age = 18;
+            Stamat.Name = "Stamat";
+            Stamat.Age = 43;
+
+
+            Console.WriteLine(Pesho.Name+ " " + Pesho.Age);
+            Console.WriteLine(Gosho.Name + " " + Gosho.Age);
+            Console.WriteLine(Stamat.Name + " " + Stamat.Age);
+
+        }
+    }
+}
