c0bb6eb866b25f93100b4decdc5ccc14d817d4d9
Andrey <55507306+Andrey5kot@users.noreply.github.com>
2019-10-27 19:36:53 +0200

    Add files via upload

---
+++ laba_12/5_King’s_Gambit_Extended/King.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5_King_s_Gambit_Extended
+{
+    class King
+    {
+        public King(string name)
+        {
+            this.Name = name;
+        }
+
+        public event EventHandler BeingAttacked;
+
+        public string Name { get; set; }
+
+        public void TakeAttack()
+        {
+            this.OnBeingAttacked();
+        }
+
+        protected virtual void OnBeingAttacked()
+        {
+            Console.WriteLine($"King {this.Name} is under attack!");
+            BeingAttacked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ laba_12/5_King’s_Gambit_Extended/Royal Guard.cs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5_King_s_Gambit_Extended
+{
+    class Royal_Guard
+    {
+        public string name;
+        public int numberLives = 3;
+
+        public Royal_Guard(string name)
+        {
+            this.name = name;
+        }
+
+        public void OnKingBeingAttacked(object source, EventArgs args)
+        {
+            Console.WriteLine($"Royal Guard {this.name} is defending!");
+        }
+    }
+}
